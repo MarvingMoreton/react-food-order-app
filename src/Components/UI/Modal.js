0b4bfd05af28c817e6d0062eb04f3fce49afc082
Marving Moreton <moreton.marving@gmail.com>
2022-10-02 16:59:51 -0400

    shopping cart element + Modal via React Portal

---
+++ src/Components/UI/Modal.js
@@ -0,0 +1,31 @@
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+import React from "react";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+//Helper function to inform the Portal where to insert the Data:
+const portalElement = document.getElementById("overlays");
+
+function Modal(props) {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </React.Fragment>
+  );
+}
+
+export default Modal;
