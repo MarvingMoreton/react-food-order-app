780fe291dea2003addbdd2ea7c21a176e7e67137
Marving Moreton <moreton.marving@gmail.com>
2022-10-02 17:20:30 -0400

    Maning Cart & Modal State

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
 
 function App() {
+  // Managing Cart & Modal State when pop up visible or not
+  const [cartIsShown, setCartIsShown] = React.useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
